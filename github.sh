d554fe0e49de2e12d6333291f1aa9c8798e7e077
Yassine Nassibi <ynassibi@e3r7p11.1337.ma>
2024-01-16 16:25:29 +0100

    just test script github

--- github.sh
+++ github.sh
@@ -6,16 +6,27 @@
 #    By: ynassibi <ynassibi@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/01/16 16:14:15 by ynassibi          #+#    #+#              #
-#    Updated: 2024/01/16 16:17:56 by ynassibi         ###   ########.fr        #
+#    Updated: 2024/01/16 16:24:56 by ynassibi         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 
 input=$1;
-git add .
+if [ "$1" == "s" ]; then
+    git status -s
+elif [ "$1" == "c" ]; then
+    git commit -m "$2"
+elif [ "$1" == "a" ]; then
+    git add  "$2"
+elif [ "$1" == "p" ]; then
+    git push
+elif [ "$1" == "all" ]; then
+	git add .
+	git status -s
+	git commit -m "$2"
+	git push
+else
+    echo "Invalid argument."
+fi
 
-git statud -s
-git commit -m "$input"
-
-git push
