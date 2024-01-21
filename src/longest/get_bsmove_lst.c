c49921edd1cad25c4813ef6384907f6dceccfcac
Yassine Nassibi <ynassibi@e3r7p12.1337.ma>
2024-01-21 14:05:55 +0100

    makhdmatchs

--- src/longest/get_bsmove_lst.c
+++ src/longest/get_bsmove_lst.c
@@ -6,7 +6,7 @@
 /*   By: ynassibi <ynassibi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 10:00:38 by ynassibi          #+#    #+#             */
-/*   Updated: 2024/01/21 14:01:46 by ynassibi         ###   ########.fr       */
+/*   Updated: 2024/01/21 14:04:54 by ynassibi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,18 @@ void	get_bsmove_lst(t_stack **sa, t_stack **sb)
 {
 
 	nsub_to_sb(sa, sb);
-	int i = get_lstsize(*sb);
 	get_lsthooks(*sa, *sb);
-	while (i > 0)
+	while (get_lstsize(*sb))
 	{
 		get_lsthooks(*sa, *sb);
+		set_pos_lstx(*sa);
+		set_pos_lstx(*sb);
 		bolt(*sb, *sa);
 		getmin_move(sb,  sa);
 		loop(sa,sb);
-		pu_sh(sb, sa,1);
-		i--;
+		if (!(*sb))
+			break;
+		push_a(sa, sb);
 	}
 
 }
